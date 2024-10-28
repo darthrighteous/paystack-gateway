2637abe8a4649e77971fdb4ae59624f5f8c2f84b
darthrighteous <ogunniyiabduljalil@yahoo.com>
2024-10-29 00:16:16 +0100

    Add Customers api module

--- lib/paystack_gateway.rb
+++ lib/paystack_gateway.rb
@@ -6,6 +6,7 @@ require 'paystack_gateway/api_error'
 require 'paystack_gateway/request_module'
 require 'paystack_gateway/response'
 require 'paystack_gateway/miscellaneous'
+require 'paystack_gateway/customers'
 
 # = PaystackGateway
 module PaystackGateway
---
+++ lib/paystack_gateway/customers.rb
@@ -0,0 +1,44 @@
+# frozen_string_literal: true
+
+module PaystackGateway
+  # Create and manage customers https://paystack.com/docs/api/customer/
+  module Customers
+    include PaystackGateway::RequestModule
+
+    # Common helpers for response from customer endpoints
+    module CustomerResponse
+      extend ActiveSupport::Concern
+
+      included do
+        delegate :id, :customer_code, to: :data
+      end
+    end
+
+    class CreateCustomerResponse < PaystackGateway::Response
+      include CustomerResponse
+    end
+
+    api_method def self.create_customer(email:, first_name:, last_name:)
+      with_response(CreateCustomerResponse) do |connection|
+        connection.post('/customer', { email:, first_name:, last_name: })
+      end
+    end
+
+    # Response from GET /customer/:email_or_id
+    class FetchCustomerResponse < PaystackGateway::Response
+      include CustomerResponse
+
+      delegate :subscriptions, :authorizations, to: :data
+
+      def active_subscriptions = subscriptions.select { _1.status == 'active' }
+      def active_subscription_codes = active_subscriptions.map(&:subscription_code)
+      def reusable_authorizations = authorizations.select(&:reusable)
+    end
+
+    api_method def self.fetch_customer(email:)
+      with_response(FetchCustomerResponse) do |connection|
+        connection.get("/customer/#{email}")
+      end
+    end
+  end
+end
