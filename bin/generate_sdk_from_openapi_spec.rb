d16a5ac28e503313d9ff3e825fbcfde700f9f636
darthrighteous <ogunniyiabduljalil@yahoo.com>
2025-03-30 01:10:23 +0100

    Update script to define entire module body in a single method

--- bin/generate_sdk_from_openapi_spec.rb
+++ bin/generate_sdk_from_openapi_spec.rb
@@ -13,221 +13,133 @@ INDENT = '  '
 WRAP_LINE_LENGTH = 80
 TOP_LEVEL_RESPONSE_KEYS = %w[status message data meta].freeze
 
-def api_methods_by_api_module_name
-  @api_methods_by_api_module_name ||=
-    document.paths.path.each_with_object({}) do |(path, path_item), paths_by_api_module|
-      %w[get post put patch delete].each do |http_method|
-        operation = path_item.operation(http_method)
-        next if !operation
-
-        api_module_name = operation.tags.first.remove(' ')
-        next if !api_module_name
-
-        paths_by_api_module[api_module_name] ||= []
-        paths_by_api_module[api_module_name] << { path:, http_method:, operation: }
-      end
-    end
-end
-
-def update_required_files(module_names)
-  file_lines = File.readlines(LIB_FILE)
-
-  comment_start_index = file_lines.find_index { |line| line.include?('# API Modules') }
-  if !comment_start_index
-    puts "Could not find '# API Modules' comment in #{LIB_FILE}"
-    return
-  end
-
-  insertion_start = comment_start_index + 1
-  insertion_start += 1 while insertion_start < file_lines.size && file_lines[insertion_start].start_with?('#')
-
-  insertion_end = insertion_start
-  insertion_end += 1 while insertion_end < file_lines.size && !file_lines[insertion_end].strip.empty?
-
-  file_lines[insertion_start...insertion_end] =
-    module_names.map { |module_name| "require 'paystack_gateway/#{module_name.underscore}'\n" }.sort.join
-
-  File.write(LIB_FILE, file_lines.join)
-
-  puts 'Updated paystack_gateway.rb with new API Modules requires.'
-end
-
-def api_module_content(api_module_name, api_methods)
+def api_module_content(api_module_name, api_methods) # rubocop:disable Metrics/AbcSize,Metrics/MethodLength,Metrics/CyclomaticComplexity,Metrics/PerceivedComplexity
+  module_info = tags_by_name[api_module_name]
   <<~RUBY
     # frozen_string_literal: true
 
     module PaystackGateway
-      #{api_module_docstring(api_module_name)}
+      # https://paystack.com/docs/api/#{api_module_name.parameterize}
+      #
+      # #{module_info['x-product-name'].squish || api_module_name.humanize}
+      # #{module_info['description'].squish}
       module #{api_module_name}
         include PaystackGateway::RequestModule
 
-    #{api_methods.map { |info| api_method_composition(api_module_name, info) }.join("\n").chomp}
+    #{
+      api_methods.map do |api_method_info| # rubocop:disable Metrics/BlockLength
+        api_method_info => { path:, http_method:, operation: }
+
+        api_method_name = api_method_name(api_module_name, operation)
+        <<~RUBY.chomp
+          #{INDENT * 2}# Successful response from calling ##{api_method_name}.
+          #{INDENT * 2}class #{"#{api_method_name}_response".camelize} < PaystackGateway::Response#{
+            if (data_keys = api_response_data_keys(operation)).any?
+              if data_keys.length > 3
+                "\n#{INDENT * 3}delegate #{data_keys.map { |key| ":#{key}," }.join("\n#{INDENT * 3} #{' ' * 'delegate'.length}")} to: :data\n#{INDENT * 2}end"
+              else
+                "\n#{INDENT * 3}delegate #{data_keys.map { |key| ":#{key}" }.join(', ')}, to: :data\n#{INDENT * 2}end"
+              end
+            else
+              '; end'
+            end
+          }
+
+          #{INDENT * 2}# Error response from ##{api_method_name}.
+          #{INDENT * 2}class #{"#{api_method_name}_error".camelize} < ApiError; end
+
+          #{INDENT * 2}# https://paystack.com/docs/api/#{operation.tags.first.parameterize}/##{api_method_name}
+          #{INDENT * 2}# #{operation.summary}: #{http_method.upcase} #{path}#{
+            operation.description ? "\n#{INDENT * 2}# #{operation.description}" : ''
+          }
+          #{INDENT * 2}##{
+            docstring = ''
+            api_method_parameters(operation).each do |param|
+              docstring += "\n#{INDENT * 2}# @param #{param[:name]} [#{param[:type]}]"
+              docstring += ' (required)' if param[:required]
+
+              if (description = param[:description]&.squish)
+                docstring += wrapped_text(description, "\n#{INDENT * 2}##{INDENT * 3} ")
+              end
+
+              next if !(object_properties = param[:object_properties])
+
+              object_properties.each do |props|
+                docstring += "\n#{INDENT * 2}##{INDENT * 1} @option #{param[:name]} [#{props[:type]}] :#{props[:name]}"
+                docstring += wrapped_text(props[:description], "\n#{INDENT * 2}##{INDENT * 5}").to_s
+              end
+            end
+
+            docstring
+          }
+          #{INDENT * 2}#
+          #{INDENT * 2}# @return [#{"#{api_method_name}_response".camelize}] successful response
+          #{INDENT * 2}# @raise [#{"#{api_method_name}_error".camelize}] if the request fails
+          #{INDENT * 2}api_method def self.#{api_method_name}#{
+            if (method_args = api_method_args(operation)).any?
+              if method_args.length > 5
+                "(#{method_args.map { |arg| "\n#{INDENT * 3}#{arg}" }.join(',')}\n#{INDENT * 2})"
+              else
+                "(#{method_args.join(', ')})"
+              end
+            end
+          }
+          #{INDENT * 2}  use_connection do |connection|
+          #{INDENT * 2}    connection.#{http_method}(
+          #{INDENT * 2}      #{
+            if path == (interpolated = path.gsub(/{([^}]+)}/, "\#{\\1}"))
+              "'#{path}'"
+            else
+              "\"#{interpolated}\""
+            end
+          },#{
+            if (request_args = api_request_args(operation)).any?
+              if request_args.length > 5
+                "\n#{INDENT * 5}{#{request_args.map { |arg| "\n#{INDENT * 6}#{arg}," }.join}\n#{INDENT * 5}}.compact,"
+              else
+                "\n#{INDENT * 5}{ #{request_args.join(', ')} }.compact,"
+              end
+            end
+          }
+          #{INDENT * 2}    )
+          #{INDENT * 2}  end
+          #{INDENT * 2}end
+
+        RUBY
+      end.join("\n").chomp
+    }
       end
     end
   RUBY
 end
 
-def api_module_docstring(api_module_name)
-  module_info = tags_by_name[api_module_name]
-  return if !module_info
-
-  docstring = "# https://paystack.com/docs/api/#{api_module_name.parameterize}"
-  docstring += "\n#{INDENT * 1}#"
-  docstring += "\n#{INDENT * 1}# #{module_info['x-product-name'].squish || api_module_name.humanize}"
-
-  docstring += "\n#{INDENT * 1}# #{module_info['description'].squish}" if module_info['description']
-  docstring
-end
-
-def api_method_composition(api_module_name, api_method_info)
-  api_method_info => { path:, http_method:, operation: }
-
-  api_method_name = api_method_name(api_module_name, operation)
-  <<~RUBY.chomp
-    #{api_method_response_class_content(api_method_name, operation)}
-
-    #{api_method_error_class_content(api_method_name)}
-
-    #{api_method_content(api_method_name, operation, http_method, path)}
-  RUBY
-end
-
-def api_method_response_class_content(api_method_name, operation)
-  definition = "#{INDENT * 2}# Successful response from calling ##{api_method_name}.\n" \
-               "#{INDENT * 2}class #{"#{api_method_name}_response".camelize} < PaystackGateway::Response"
-
-  if (delegate_content = api_method_response_class_delegate_content(operation))
-    "#{definition}#{delegate_content}\n#{INDENT * 2}end"
-  else
-    "#{definition}; end"
-  end
-end
-
-def api_method_response_class_delegate_content(operation)
+def api_response_data_keys(operation)
   responses = operation.responses.response
   success_response = responses[responses.keys.find { _1.match?(/\A2..\z/) }]
-  return if !success_response
+  return [] if !success_response
 
   required_data_keys = success_response.content['application/json'].schema.properties['data']&.required || []
-
-  required_data_keys -= TOP_LEVEL_RESPONSE_KEYS
-  return if required_data_keys.none?
-
-  if required_data_keys.length > 3
-    definition = "\n#{INDENT * 3}delegate :#{required_data_keys.shift},"
-
-    while (line_key = required_data_keys.shift).present?
-      definition += "\n#{INDENT * 3}#{' ' * 'delegate'.length} :#{line_key},"
-    end
-
-    "#{definition} to: :data"
-  else
-    "\n#{INDENT * 3}delegate #{required_data_keys.map { |key| ":#{key}" }.join(', ')}, to: :data"
-  end
+  required_data_keys - TOP_LEVEL_RESPONSE_KEYS
 end
 
-def api_method_error_class_content(api_method_name)
-  "#{INDENT * 2}# Error response from ##{api_method_name}.\n" \
-    "#{INDENT * 2}class #{"#{api_method_name}_error".camelize} < ApiError; end"
-end
-
-def api_method_content(api_method_name, operation, http_method, path)
-  <<-RUBY
-    #{api_method_definition_header_docstring(api_method_name, operation, http_method, path)}
-    #{api_method_definition_params_docstring(operation)}
-    #{api_method_definition_response_docstring(api_method_name)}
-    #{api_method_definition_name_and_parameters(api_method_name, operation)}
-      use_connection do |connection|
-        connection.#{http_method}(
-          #{api_method_definition_path(path)},#{api_method_definition_request_params(operation)}
-        )
-      end
-    end
-  RUBY
-end
-
-def api_method_definition_header_docstring(api_method_name, operation, http_method, path)
-  docstring = "# https://paystack.com/docs/api/#{operation.tags.first.parameterize}/##{api_method_name}"
-  docstring += "\n#{INDENT * 2}# #{operation.summary}: #{http_method.upcase} #{path}"
-  docstring += "\n#{INDENT * 2}# #{operation.description}" if operation.description.present?
-  docstring
-end
-
-def api_method_definition_params_docstring(operation)
-  docstring = '#'
-
-  api_method_parameters(operation).each do |param|
-    docstring += "\n#{INDENT * 2}# @param #{param[:name]} [#{param[:type]}]"
-    docstring += ' (required)' if param[:required]
-
-    if (description = param[:description]&.squish)
-      docstring += wrapped_text(description, "\n#{INDENT * 2}##{INDENT * 3} ")
-    end
-
-    next if !(object_properties = param[:object_properties])
-
-    object_properties.each do |props|
-      docstring += "\n#{INDENT * 2}##{INDENT * 1} @option #{param[:name]} [#{props[:type]}] :#{props[:name]}"
-      docstring += wrapped_text(props[:description], "\n#{INDENT * 2}##{INDENT * 5}")
-    end
-  end
-
-  docstring
-end
-
-def api_method_definition_response_docstring(api_method_name)
-  docstring = '#'
-  docstring += "\n#{INDENT * 2}# @return [#{"#{api_method_name}_response".camelize}] successful response"
-  docstring + "\n#{INDENT * 2}# @raise [#{"#{api_method_name}_error".camelize}] if the request fails"
-end
-
-def api_method_definition_name_and_parameters(api_method_name, operation)
-  method_args = api_method_parameters(operation).map do |param|
+def api_method_args(operation)
+  api_method_parameters(operation).map do |param|
     name = param[:name].underscore
     param[:required] ? "#{name}:" : "#{name}: nil"
   end
-
-  definition = "api_method def self.#{api_method_name}"
-  return definition if method_args.none?
-
-  definition += '('
-
-  if method_args.length > 5
-    while (line_arg = method_args.shift).present?
-      definition += "\n#{INDENT * 3}#{line_arg}"
-      definition += ',' if method_args.any?
-    end
-    definition + "\n#{INDENT * 2})"
-  else
-    definition + "#{method_args.join(', ')})"
-  end
 end
 
-def api_method_definition_path(path)
-  # "/transaction/verify/{reference}" -> "/transaction/verify/#{reference}"
-  interpolated_path = path.gsub(/{([^}]+)}/, "\#{\\1}")
+def api_request_args(operation)
+  api_method_parameters(operation)
+    .filter_map do |param|
+      next if param[:in] == 'path'
 
-  interpolated_path == path ? "'#{interpolated_path}'" : "\"#{interpolated_path}\""
-end
-
-def api_method_definition_request_params(operation)
-  params = api_method_parameters(operation)
-             .reject { |param| param[:in] == 'path' }
-             .map { |param| param[:name] }
-  return if params.none?
-
-  definition = "\n#{INDENT * 5}{"
-
-  if params.length > 5
-    while (line_param = params.shift).present?
-      definition += "\n#{INDENT * 6}#{line_param}:" \
-                    "#{line_param == line_param.underscore ? nil : " #{line_param.underscore}"},"
+      if param[:name] == param[:name].underscore
+        "#{param[:name]}:"
+      else
+        "#{param[:name]}: #{param[:name].underscore}"
+      end
     end
-
-    definition + "\n#{INDENT * 5}}.compact,"
-  else
-    "#{definition} #{params.map { |param| "#{param}:" }.join(', ')} }.compact,"
-  end
 end
 
 def api_method_parameters(operation)
@@ -365,6 +277,22 @@ def document
   @document ||= OpenAPIParser.parse(YAML.load_file(OPENAPI_SPEC), strict_reference_validation: true)
 end
 
+def api_methods_by_api_module_name
+  @api_methods_by_api_module_name ||=
+    document.paths.path.each_with_object({}) do |(path, path_item), paths_by_api_module|
+      %w[get post put patch delete].each do |http_method|
+        operation = path_item.operation(http_method)
+        next if !operation
+
+        api_module_name = operation.tags.first.remove(' ')
+        next if !api_module_name
+
+        paths_by_api_module[api_module_name] ||= []
+        paths_by_api_module[api_module_name] << { path:, http_method:, operation: }
+      end
+    end
+end
+
 module_names = api_methods_by_api_module_name.map do |api_module_name, api_methods|
   if existing_api_modules.include?(api_module_name)
     puts "Updating existing module: #{api_module_name}"
@@ -381,4 +309,23 @@ module_names = api_methods_by_api_module_name.map do |api_module_name, api_metho
   api_module_name
 end
 
-update_required_files(module_names)
+lib_file_lines = File.readlines(LIB_FILE)
+
+comment_start_index = lib_file_lines.find_index { |line| line.include?('# API Modules') }
+if !comment_start_index
+  puts "Could not find '# API Modules' comment in #{LIB_FILE}"
+  exit 1
+end
+
+insertion_start = comment_start_index + 1
+insertion_start += 1 while insertion_start < lib_file_lines.size && lib_file_lines[insertion_start].start_with?('#')
+
+insertion_end = insertion_start
+insertion_end += 1 while insertion_end < lib_file_lines.size && !lib_file_lines[insertion_end].strip.empty?
+
+lib_file_lines[insertion_start...insertion_end] =
+  module_names.map { |module_name| "require 'paystack_gateway/#{module_name.underscore}'\n" }.sort.join
+
+File.write(LIB_FILE, lib_file_lines.join)
+
+puts 'Updated paystack_gateway.rb with new API Modules requires.'
